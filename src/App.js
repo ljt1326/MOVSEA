6f98fe50b831f0d1d1df30eeeed2726eb6440b21
Lapis Laz <ljt1326@naver.com>
2021-04-08 04:32:12 +0900

    0408 try

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Potato from "./Potato";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Potato />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState({count: this.state.count + 1 });
+  };
+  minus = () => {
+    this.setState({count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-export default App;
+  export default App;
